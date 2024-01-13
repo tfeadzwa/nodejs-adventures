3a53565dfdc1e338d57d9ce95f9d46ef48c49d9b
tfeadzwa <tfadzwa02@gmail.com>
2024-01-13 23:35:57 +0200

    add error logs and request server logs

--- webserver-02tut/logEvents.js
+++ webserver-02tut/logEvents.js
@@ -5,7 +5,7 @@ const path = require("path");
 const fs = require("fs");
 const fsPromises = require("fs").promises;
 
-const logEvents = async (message) => {
+const logEvents = async (message, fileName) => {
   const dateTime = `${format(new Date(), "yyyy-MM-dd\tHH:mm:ss")}`;
   const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
   console.log(logItem);
@@ -15,7 +15,7 @@ const logEvents = async (message) => {
       await fsPromises.mkdir(path.join(__dirname, "logs"));
     }
     await fsPromises.appendFile(
-      path.join(__dirname, "logs", "eventLog.txt"),
+      path.join(__dirname, "logs", fileName),
       logItem
     );
   } catch (err) {
--- webserver-02tut/server.js
+++ webserver-02tut/server.js
@@ -2,18 +2,43 @@ const http = require("http");
 const fs = require("fs");
 const fsPromises = require("fs").promises;
 const path = require("path");
-
-const EventEmitter = require("events");
 const logEvents = require("./logEvents");
+const EventEmitter = require("events");
 
 class Emitter extends EventEmitter {}
 
 const myEmitter = new Emitter();
 
+// create a serve
 const PORT = process.env.PORT || 3500;
 
+myEmitter.on("logs", (msg, fileName) => logEvents(msg, fileName));
+
+const serverFile = async (contentType, filePath, response) => {
+  try {
+    const rawData = await fsPromises.readFile(
+      filePath,
+      !contentType.includes("image") ? "utf8" : ""
+    );
+    const data =
+      contentType === "application/json" ? JSON.parse(rawData) : rawData;
+    response.writeHead(filePath.includes("404.html") ? 404 : 200, {
+      "Content-Type": contentType,
+    });
+    response.end(
+      contentType === "application/json" ? JSON.stringify(data) : data
+    );
+  } catch (err) {
+    console.log(err);
+    myEmitter.emit("logs", `${err.name}: ${err.message}`, "errorLog.txt");
+    response.statusCode = 500;
+    response.end();
+  }
+};
+
 const server = http.createServer((req, res) => {
-  console.log(`${req.url}\t${req.method}`);
+  console.log(`${req.url} ${req.method}`);
+  myEmitter.emit("logs", `${req.url}\t${req.method}`, "reqLog.txt");
 
   const extension = path.extname(req.url);
   let contentType;
@@ -23,7 +48,7 @@ const server = http.createServer((req, res) => {
       contentType = "text/css";
       break;
     case ".js":
-      contentType = "text/javascript";
+      contentType = "application/javascript";
       break;
     case ".json":
       contentType = "application/json";
@@ -39,6 +64,7 @@ const server = http.createServer((req, res) => {
       break;
     default:
       contentType = "text/html";
+      break;
   }
 
   let filePath =
@@ -46,25 +72,31 @@ const server = http.createServer((req, res) => {
       ? path.join(__dirname, "views", "index.html")
       : contentType === "text/html" && req.url.slice(-1) === "/"
       ? path.join(__dirname, "views", req.url, "index.html")
-      : contentType === "text.html"
+      : contentType === "text/html"
       ? path.join(__dirname, "views", req.url)
       : path.join(__dirname, req.url);
 
+  // handle if req.url contains no extension
   if (!extension && req.url.slice(-1) !== "/") filePath += ".html";
 
-  const fileExits = fs.existsSync(filePath);
-
-  if (fileExits) {
-    // serve the file
+  // check if the filepath exists
+  const fileExists = fs.existsSync(filePath);
+  if (fileExists) {
+    serverFile(contentType, filePath, res);
   } else {
-    // 404 error
-    // 301 redirect
-    console.log(path.parse(filePath));
+    switch (path.parse(filePath).base) {
+      case "old-page.html":
+        response.writeHead(301, { Location: "new-page.html" });
+        response.end();
+        break;
+      case "www-page.html":
+        response.writeHead(301, { Location: "/" });
+        response.end();
+        break;
+      default:
+        serverFile("text/html", path.join(__dirname, "views", "404.html"), res);
+    }
   }
 });
 
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-
-// add event listener for log event
-/* myEmitter.on("log", (msg) => logEvents(msg));
-myEmitter.emit("log", "log event emitted!"); */
