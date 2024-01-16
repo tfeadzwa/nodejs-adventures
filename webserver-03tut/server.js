8c1a64e5f3c0a2467bf8ba7dece21e689a350b9c
tfeadzwa <tfadzwa02@gmail.com>
2024-01-16 23:44:03 +0200

    manage api request using cors

--- webserver-03tut/server.js
+++ webserver-03tut/server.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const path = require("path");
 const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
 
 const PORT = process.env.PORT || 3500;
 
@@ -52,8 +53,17 @@ app.get("/old-page(.html)?", (req, res) => {
   res.redirect(301, "/new-page.html");
 });
 
-app.get("/*", (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
