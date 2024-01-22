46590e81a2f1fc3ce44d6b3e02a0fc24ddbefe31
tfeadzwa <tfadzwa02@gmail.com>
2024-01-22 23:59:09 +0200

    Add logout route to delete refresh token
    This commit adds a logout route that deletes the refresh token from the database when the user logs out from the node and express backend. This ensures that the user session is terminated and the refresh token cannot be reused by malicious actors.

---
+++ webserver-03tut/controllers/logoutController.js
@@ -0,0 +1,41 @@
+const usersDB = {
+  users: require("../models/users.json"),
+  setUsers: function (users) {
+    this.users = users;
+  },
+};
+
+const fsPromises = require("fs").promises;
+const path = require("path");
+
+const handleUserLogout = async (req, res) => {
+  // On client also delete the access token
+
+  const cookies = req.cookies;
+  if (!cookies?.jwt)
+    return res.status(204).json({ message: "No content found!" }); // no content
+  const refreshToken = cookies.jwt;
+
+  const foundUser = usersDB.users.find(
+    (user) => user.refreshToken === refreshToken
+  );
+  if (!foundUser) {
+    res.clearCookie("jwt", { httpOnly: true, sameSite: "None", secure: true });
+    return res.status(204).json({ message: "User not found!" });
+  }
+
+  const otherUsers = usersDB.users.filter(
+    (user) => user.refreshToken !== refreshToken
+  );
+  const currentUser = { ...foundUser, refreshToken: "" };
+  usersDB.setUsers([...otherUsers, currentUser]);
+  await fsPromises.writeFile(
+    path.join(__dirname, "..", "models", "users.json"),
+    JSON.stringify(usersDB.users)
+  );
+
+  res.clearCookie("jwt", { httpOnly: true, sameSite: "None", secure: true });
+  res.status(204).json("message: user logged out!");
+};
+
+module.exports = { handleUserLogout };
